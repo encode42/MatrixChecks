106e9024a4587cc63678777ad99ec9b22b629604
Encode42 <me@encode42.dev>
2020-08-22 16:33:33 -0400

    Whoops, removed cloud.conditional checks

--- tools/minify-yaml.js
+++ tools/minify-yaml.js
@@ -16,7 +16,7 @@ const fs               = require("fs");
 
 // Defaults
 let args = process.argv.splice(2, process.argv.length + 1);
-if (args.length === 0) args = ["../checks.yml", "../conditional.checks.yml", "../error.checks.yml"];
+if (args.length === 0) args = ["../checks.yml", "../error.checks.yml", "../legacy.checks.yml"];
 
 // Minify files
 log.info(stripIndents`
